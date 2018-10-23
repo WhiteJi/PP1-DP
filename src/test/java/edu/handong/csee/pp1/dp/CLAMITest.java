598bb1d65aa6c926ed1d586d2a2c24bfefca444c
Jiho-Choi <21700748@handong.edu>
2018-10-23 22:31:01 +0900

    TASK 5 COMPELTE

--- src/test/java/edu/handong/csee/pp1/dp/CLAMITest.java
+++ src/test/java/edu/handong/csee/pp1/dp/CLAMITest.java
@@ -90,7 +90,7 @@ public void testCLAMIWithYourSelectedProject2() {
 		
 		// TASK6 TODO Use this method to find the best cutoff for f-measure
 		
-		String bestCutoff = "50";
+		String bestCutoff = "42";
 		
 		String[] args = {"-f","data/Apache.arff","-l","isDefective","-p", "TRUE", "-m", "-c", bestCutoff};
 
